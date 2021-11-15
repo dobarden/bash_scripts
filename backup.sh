d942bd61232d7729f78f576cc2f0947724fb948b
DenisZ <nawex1@gmail.com>
2021-11-15 04:29:42 +0000

    made some changes

--- backup.sh
+++ backup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#Removing old backups
+sudo find /PATH_TO_STORE_BACKUP/ -type f -mtime +6 -exec rm -rf {} \;
+
 #Create a backup copy of data base
 archive_db=DOMAIN_NAME-database-`date +%F`
 mysqldump -u USER_NAME -p'USER_PASSWORD' DB_NAME | gzip > /PATH_TO_STORE_BACKUP/$archive_db.sql.gz
@@ -8,5 +11,3 @@ mysqldump -u USER_NAME -p'USER_PASSWORD' DB_NAME | gzip > /PATH_TO_STORE_BACKUP/
 archive_files=DOMAIN_NAME-`date +%F`
 /bin/tar -czvf /PATH_TO_STORE_BACKUP/$archive_files.tar.gz  /var/www/DOMAIN_NAME/
 
-#Removing backups older than 7 days
-sudo find /PATH_TO_STORE_BACKUP/ -type f -mtime +7 -exec rm -f {} \;
